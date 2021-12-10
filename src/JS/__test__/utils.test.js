b1305ab5b503c2886e443a8e822ab687485f5565
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-12-10 12:18:51 +0100

    Add remove feature on test file

--- src/JS/__test__/utils.test.js
+++ src/JS/__test__/utils.test.js
@@ -1,8 +1,4 @@
-/**
- * @jest-environment jsdom
- */
-
-import { AssignButtons, AddTask } from '../utils.js';
+import { AssignButtons, AddTask, DeleteTask } from '../utils.js';
 import virtualDom from '../__mock__/virtualDom.js';
 import mockedLocalStorage from '../__mock__/virtualStorage.js';
 
@@ -25,5 +21,55 @@ describe('AddTask Function', () => {
 
     // Assert
     expect(list.children).toHaveLength(1);
+  });
+});
+
+describe('DeleteTask function', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: mockedLocalStorage,
+    });
+  });
+  test('Remove one task and check on the list DOM', () => {
+    // Assign
+    const list = document.querySelector('.list-content');
+    // Act
+    AssignButtons();
+    DeleteTask(1);
+    // Assert
+    expect(list.children).toHaveLength(2);
+  });
+  test('Remove every task one by one and check on the list DOM', () => {
+    // Assign
+    const list = document.querySelector('.list-content');
+    // Act
+    AssignButtons();
+    DeleteTask(1);
+    DeleteTask(1);
+    DeleteTask(1);
+    // Assert
+    expect(list.children).toHaveLength(0);
+  });
+  test('Remove one task and check on the localStorage', () => {
+    // Act
+    AssignButtons();
+    AddTask('task number 1');
+    AddTask('task number 2');
+    DeleteTask(0);
+    // Assert
+    expect(
+      JSON.parse(window.localStorage.getItem('taskArray'))[0].description,
+    ).toBe('task number 2');
+  });
+  test('Remove all tasks and check on the localStorage', () => {
+    // Act
+    AssignButtons();
+    AddTask('task number 1');
+    AddTask('task number 2');
+    DeleteTask(1);
+    DeleteTask(1);
+    DeleteTask(1);
+    // Assert
+    expect(JSON.parse(window.localStorage.getItem('taskArray'))).toHaveLength(0);
   });
 });
